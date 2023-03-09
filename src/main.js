5c17ebf9c1115f0cd188fc43b032113b79bad2cd
Matthew <65182920+Matthew-y@users.noreply.github.com>
2023-03-09 13:19:08 +0800

    Delete comments

--- src/main.js
+++ src/main.js
@@ -6,28 +6,6 @@ import axios from "./axios";
 import elementPlus from "element-plus";
 import "element-plus/dist/index.css";
 
-// router.beforeEach((to, from, next) => {
-//   const accessToken = window.localStorage.getItem("AccessToken");
-//   if (to.path === "/") {
-//     if (accessToken) {
-//       router.push({ path: "/index/admin/list" });
-//       next();
-//     } else {
-//       console.log("need login");
-//       router.push({ path: "/" });
-//       next();
-//     }
-//   } else {
-//     if (accessToken) {
-//       next();
-//     } else {
-//       console.log("need login");
-//       router.push({ path: "/" });
-//     }
-//   }
-//   next();
-// });
-
 const app = createApp(App);
 
 app.config.globalProperties.$axios = axios;
